c7762c84640fd167bab04d81f1641c912640a1cd
Ildiko Czeller <czeildi@gmail.com>
2018-01-25 18:31:38 +0100

    add wordwrap kata

--- src/dojo.R
+++ src/dojo.R
@@ -1,3 +1,27 @@
-dojo <- function() {
-    42
+library("stringr")
+
+wrap <- function(text, columnLength) {
+    if (str_length(text) <= columnLength) {
+        return(text)
+    }
+    firstLine <- getFirstLine(text, columnLength)
+    remainingText <- getRemainingText(text, firstLine)
+
+    str_c(firstLine, "\n", wrap(remainingText, columnLength))
+}
+
+getFirstLine <- function(text, columnLength) {
+    firstLineLength <- calculateFirstLineLength(text, columnLength)
+    str_sub(text, 1, firstLineLength)
+}
+
+getRemainingText <- function(text, firstLine) {
+    str_trim(str_sub(text, str_length(firstLine) + 1))
+}
+
+calculateFirstLineLength <- function(text, columnLength) {
+    firstChunkWOptionalSpace <- str_sub(text, 1, columnLength + 1)
+    spaceIndexesInFirstLine <- str_locate_all(firstChunkWOptionalSpace, " ")[[1]][, 1]
+    lastSpaceIndex <- tail(spaceIndexesInFirstLine, 1)
+    min(columnLength, lastSpaceIndex - 1)
 }
