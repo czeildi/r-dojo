cb1227ccdadca1f346d529f2dc58c2597975b70b
Ildiko Czeller <czeildi@gmail.com>
2018-02-12 18:04:41 +0100

    add pancake sort

--- src/dojo.R
+++ src/dojo.R
@@ -1,3 +1,28 @@
-dojo <- function() {
-    42
+pancake_sort <- function(input) {
+    if (length(input) < 2) {
+        return(input)
+    }
+    
+    if (isFirstTheSmallest(input)) {
+        c(input[1], pancake_sort(input[2:length(input)]) )
+    } else {
+        pancake_sort(shift_right(input))
+    }
+}
+
+isFirstTheSmallest <- function(input) {
+    input[1] == min(input)
+}
+
+shift_right <- function(input) {
+    pancake_flip(input, length(input)) %>% 
+        pancake_flip(., length(input) - 1) 
+}
+
+pancake_flip <- function(input, n) {
+    len <- length(input)
+    if (n == len) 
+        rev(input)
+    else
+        c(input[1:(len - n)], rev(input[(len - n + 1):len]))
 }
--- test/test-dojo.R
+++ test/test-dojo.R
@@ -1,6 +1,67 @@
-context("dojo")
+source("../src/dojo.R")
+
+context("pancake sort")
 
 
-test_that("dummy", {
-    expect_equal(42, dojo())
+test_that("sorted empty vector is same empty vector", {
+    expect_equal(
+        pancake_sort(numeric(0)),
+        numeric(0)
+    )
+})
+
+test_that("sorted one element vector is the same vector", {
+    expect_equal(
+        pancake_sort(c(12)),
+        c(12)
+    )
+})
+
+test_that("sorts two element vector in reversed order", {
+    expect_equal(
+        pancake_sort(c(2, 1)),
+        c(1, 2)
+    )
+})
+
+test_that("two elements in order: no change", {
+    expect_equal(
+        pancake_sort(c(1, 2)),
+        c(1, 2)
+    )
+})
+
+test_that("three elements, last two in reversed order", {
+    expect_equal(
+        pancake_sort(c(1, 3, 2)),
+        1:3
+    )
+})
+
+test_that("three elements in order: no change", {
+    expect_equal(
+        pancake_sort(1:3),
+        1:3
+    )
+})
+
+test_that("three elements in reverse order: correctly sorted", {
+    expect_equal(
+        pancake_sort(c(3, 2, 1)),
+        1:3
+    )
+})
+
+test_that("three elements in mixed order: correctly sorted", {
+    expect_equal(
+        pancake_sort(c(3, 1, 2)),
+        1:3
+    )
+})
+
+test_that("very complicated case: works on not small input", {
+    expect_equal(
+        pancake_sort(c(3, 8, 4, 1, 7, 5, 2, 6)),
+        1:8
+    )
 })
