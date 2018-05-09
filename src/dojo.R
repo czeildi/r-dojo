558476c6bb97ae52a1985d8a7dd16cf8e02b5b00
Ildiko Czeller <czeildi@gmail.com>
2018-05-09 15:25:32 +0200

    last digit of power dojo

--- src/dojo.R
+++ src/dojo.R
@@ -1,1 +1,21 @@
 library("stringr")
+
+lastDigitOfPower <- function(base, exponent){
+    exponent <- getSmallestExponentWithSameDigit(base, exponent)
+    
+    last_digit_of_base = base %% 10
+    (last_digit_of_base ^ exponent) %% 10
+}
+
+getSmallestExponentWithSameDigit <- function(base, exponent) {
+    if (exponent > 10) {
+        if(base %in% c(1, 5, 6)){
+            exponent <- 1
+        }
+        else if(base %in% c(4, 9)){
+            exponent <- exponent %% 2
+        }
+        
+    }
+    exponent
+}
--- test/test-dojo.R
+++ test/test-dojo.R
@@ -1,7 +1,35 @@
 source("../src/dojo.R")
 
-context("molekule to atoms")
+context("last digit of power")
 
-test_that("dummy", {
-    expect_true(TRUE)
+test_that("anything on the zeroth returns one", {
+    expect_equal(lastDigitOfPower(base = 5, exponent = 0), 1)
+})
+
+test_that("any integer less than 10 on the first returns the last digit of the base", {
+    expect_equal(lastDigitOfPower(base = 2, exponent = 1), 2)
+})
+
+test_that("anything on the first returns the last digit of the base", {
+    expect_equal(lastDigitOfPower(base = 23, exponent = 1), 3)
+})
+
+test_that("returns last digit of power if small number is squared", {
+    expect_equal(lastDigitOfPower(base = 23, exponent = 2), 9)
+})
+
+test_that("returns last digit of power if big number is squared", {
+    expect_equal(lastDigitOfPower(base = 233333333, exponent = 2), 9)
+})
+
+test_that("returns last digit of power if power of base's last digit is >10", {
+    expect_equal(lastDigitOfPower(base = 3, exponent = 4), 1)
+})
+
+test_that("returns last digit of power if exponent is a big number", {
+    expect_equal(lastDigitOfPower(base = 5, exponent = 100), 5)
+})
+
+test_that("returns last digit of power if exponent is a big number and digit has longer period", {
+    expect_equal(lastDigitOfPower(base = 9, exponent = 100), 1)
 })
