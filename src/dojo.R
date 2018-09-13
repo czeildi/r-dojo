0df49a49aad3b3b13c80ee1b0b871059df39d5c2
Ildiko Czeller <ildiko.czeller@emarsys.com>
2018-09-13 09:18:31 +0200

    add solution

--- src/dojo.R
+++ src/dojo.R
@@ -1,1 +1,22 @@
 library("stringr")
+library("data.table")
+
+roman_digits <- data.table(
+    arabic = c(1, 4, 5),
+    roman = c("I", "IV", "V")
+)
+
+to_roman <- function(number) {
+    if (number == 0) {
+        return("")
+    }
+    largest_digit <- largest_usable_digit(number)
+    paste0(
+        roman_digits[arabic == largest_digit, roman],
+        to_roman(number - largest_digit)
+    )
+}
+
+largest_usable_digit <- function(number) {
+    max(roman_digits[arabic <= number, arabic])
+}
--- test/test-dojo.R
+++ test/test-dojo.R
@@ -2,6 +2,16 @@ source("../src/dojo.R")
 
 context("dojo")
 
-test_that("dummy", {
-    expect_true(TRUE)
+test_cases <- list(
+    list(1, "I"),
+    list(2, "II"),
+    list(5, "V"),
+    list(4, "IV"),
+    list(6, "VI")
+)
+
+purrr::walk(test_cases, ~{
+    test_that(glue::glue("{.x[[1]]} in roman is {.x[[2]]}"), {
+        expect_equal(to_roman(.x[[1]]), .x[[2]])
+    })    
 })
