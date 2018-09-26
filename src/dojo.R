7b2375e60126d6b4c4b68d681b848c200a2acbc4
Ildiko Czeller <czeildi@gmail.com>
2018-09-26 09:00:49 +0200

    add alternating encrypt

--- src/dojo.R
+++ src/dojo.R
@@ -1,1 +1,31 @@
 library("stringr")
+
+encrypt <- function(text, times) {
+    once_encrypted <- encrypt_once(text)
+    if (times == 1) {
+        return(once_encrypted)
+    }
+    encrypt(once_encrypted, times - 1)
+}
+
+encrypt_once <- function(text){
+    encrypted_text <- purrr::map(
+        c(0, 1),
+        function(remainder) {
+            subset_text_by_remainders(text, remainder)
+        }
+    ) %>% 
+        unlist() %>% 
+        paste(collapse = "")
+}
+
+subset_text_by_remainders <- function(text, remainder){
+    split_text_to_characters(text) %>% 
+        .[(1:length(.)) %% 2 == remainder] %>% 
+        paste(collapse = "")
+}
+
+
+split_text_to_characters <- function (text) {
+    str_split(text, "")[[1]]
+}
--- test/test-dojo.R
+++ test/test-dojo.R
@@ -2,6 +2,40 @@ source("../src/dojo.R")
 
 context("dojo")
 
-test_that("dummy", {
-    expect_true(TRUE)
+test_that("encrypted 1 character string returns itself", {
+    expect_equal(
+        encrypt(text = "g", 1),
+        "g"
+    )
+})
+
+
+test_that("encrypted 2 character string returns itself reversed after one iteration", {
+    expect_equal(
+        encrypt(text = "ga", 1),
+        "ag"
+    )
+})
+
+test_that("abc encrpyted once returns bac", {
+    expect_equal(
+        encrypt(text = "abc", 1),
+        "bac"
+    )    
+})
+
+
+test_that("longer encrpyted text is correct", {
+    expect_equal(
+        encrypt(text = "This is a test!", 1),
+        "hsi  etTi sats!"
+    )    
+})
+
+
+test_that("encryption is repeated by given times", {
+    expect_equal(
+        encrypt(text = "This is a test!", 3),
+        " Tah itse sits!"
+    )    
 })
