b48a4a60555b9f2a76dd24bdc96953ca6c416c5e
Ildiko Czeller <czeildi@gmail.com>
2018-11-10 16:03:13 +0100

    reindent ops

--- R/dojo.R
+++ R/dojo.R
@@ -1,1 +1,32 @@
 library("stringr")
+
+reformat <- function(text) {
+    if (length(text) < 2) {
+        return(text)
+    }
+    longest_lhs <- max(lengthOfLhs(text), na.rm = TRUE)
+    map_chr(text, ~reformatLine(.x, longest_lhs))
+}
+
+reformatLine <- function(line, longest_lhs) {
+    if (!str_detect(line, "=")) {
+        return(line)
+    }
+    current_lhs <- lengthOfLhs(line)
+    spaces_to_insert <- createSpacesToInsert(longest_lhs, current_lhs)
+    insertSpacesBeforeOperator(line, spaces_to_insert)
+}
+
+
+insertSpacesBeforeOperator <- function(line, spaces_to_insert) {
+    str_replace(line, "=", paste0(spaces_to_insert, "="))
+}
+
+createSpacesToInsert <- function(longest_lhs, current_lhs) {
+    num_spaces_needed <- longest_lhs - current_lhs
+    paste(rep(" ", num_spaces_needed), collapse = "")
+}
+
+lengthOfLhs <- function(lines) {
+    str_locate(lines, "=")[, 1] - 1
+}
--- tests/testthat/test-dojo.R
+++ tests/testthat/test-dojo.R
@@ -1,5 +1,57 @@
 context("dojo")
 
-test_that("dummy", {
-    expect_true(TRUE)
+test_that("if code consists of single line, no reformat needed", {
+    expect_equal(
+        reformat("alma"),
+        "alma"
+    )
+})
+
+test_that("if second line needs an extra space, one space inserted", {
+    expect_equal(
+        reformat(c("korte = korte", 
+                   "alma = alma")),
+        c("korte = korte", 
+          "alma  = alma")
+    )
+})
+
+test_that("if second line needs extra spaces, correct number of spaces inserted", {
+    expect_equal(
+        reformat(c("korte = korte", 
+                   "a = alma")),
+        c("korte = korte", 
+          "a     = alma")
+    )
+})
+
+test_that("if first line needs extra spaces, correct number of spaces inserted", {
+    expect_equal(
+        reformat(c("a = alma",
+                   "korte = korte")),
+        c("a     = alma",
+          "korte = korte")
+    )
+})
+
+test_that("if text consist of multiple lines containing =, spaces are inserted to all lines", {
+    expect_equal(
+        reformat(c("a = alma",
+                   "korte = korte",
+                   "be = ki")),
+        c("a     = alma",
+          "korte = korte",
+          "be    = ki")
+    )
+})
+
+test_that("if text consist of multiple lines, not all containing =, spaces are inserted to the correct lines", {
+    expect_equal(
+        reformat(c("a = alma",
+                   "korte = korte",
+                   "be ki")),
+        c("a     = alma",
+          "korte = korte",
+          "be ki")
+    )
 })
