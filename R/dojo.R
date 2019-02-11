d2ef7ddaa614646d9eccc1402e04f6d573b4f4eb
Ildiko Czeller <czeildi@gmail.com>
2019-02-11 08:11:21 +0100

    add rectangle solution

--- R/dojo.R
+++ R/dojo.R
@@ -1,1 +1,28 @@
+areaOfOverlap <- function(rectangleA, rectangleB) {
+    if (isPoint(rectangleA) | isPoint(rectangleB)) {
+        0
+    } else if (isTRUE(all.equal(rectangleA, rectangleB))){
+        areaOfRectangle(rectangleA)
+    } else {
+        xBl <- max(rectangleA[["bl"]]["x"], rectangleB[["bl"]]["x"])
+        xTr <- min(rectangleA[["tr"]]["x"], rectangleB[["tr"]]["x"])
+        areaOfRectangle(
+            list(
+                "bl" = c(x = xBl, y = rectangleA[["bl"]]["y"]), 
+                "tr" = c(x = xTr, y = rectangleB[["tr"]]["y"])
+            )
+        )
+    }
+}
 
+isPoint <- function(rectangle) {
+    isTRUE(all.equal(rectangle[["bl"]], rectangle[["tr"]]))
+}
+
+areaOfRectangle <- function(rectangle) {
+    lengthOfSide(rectangle, "x") * lengthOfSide(rectangle, "y")
+}
+
+lengthOfSide <- function(rectangle, axis) {
+    as.numeric(rectangle[["tr"]][axis] - rectangle[["bl"]][axis])
+}
--- tests/testthat/test-dojo.R
+++ tests/testthat/test-dojo.R
@@ -1,5 +1,92 @@
 context("dojo")
 
-test_that("dummy", {
-    expect_true(TRUE)
+test_that("if one rectangle is a point, overlap area is 0", {
+    expect_equal(
+        areaOfOverlap(
+            list("bl" = c(x = 0, y = 0), "tr" = c(x = 0, y = 0)),
+            list("bl" = c(x = 3, y = 0), "tr" = c(x = 7, y = 6))
+        ),
+        0
+    )
+})
+
+test_that("if all of the rectangles is a point, the area is zero", {
+    expect_equal(
+        areaOfOverlap(
+            list("bl" = c(x = 0, y = 0), "tr" = c(x = 0, y = 0)),
+            list("bl" = c(x = 0, y = 0), "tr" = c(x = 0, y = 0))
+        ),
+        0
+    )
+})
+
+test_that("if the two rectange is the same unit square, the overlap area is the area of one of the rectangles ", {
+    expect_equal(
+        areaOfOverlap(
+            list("bl" = c(x = 0, y = 0), "tr" = c(x = 1, y = 1)),
+            list("bl" = c(x = 0, y = 0), "tr" = c(x = 1, y = 1))
+        ),
+        1
+    )
+})
+
+
+test_that("if any of the rectangles is a point, the area is zero", {
+    expect_equal(
+        areaOfOverlap(
+            list("bl" = c(x = 0, y = 0), "tr" = c(x = 1, y = 1)),
+            list("bl" = c(x = 0, y = 0), "tr" = c(x = 0, y = 0))
+        ),
+        0
+    )
+})
+
+test_that("if the two rectangle is the same origo based square, the overlap area is the area of one of the rectangles", {
+    expect_equal(
+        areaOfOverlap(
+            list("bl" = c(x = 0, y = 0), "tr" = c(x = 2, y = 2)),
+            list("bl" = c(x = 0, y = 0), "tr" = c(x = 2, y = 2))
+        ),
+        4
+    )
+})
+
+test_that("if the two rectangle is the same square, the overlap area is the area of one of the rectangles", {
+    expect_equal(
+        areaOfOverlap(
+            list("bl" = c(x = 1, y = 1), "tr" = c(x = 3, y = 3)),
+            list("bl" = c(x = 1, y = 1), "tr" = c(x = 3, y = 3))
+        ),
+        4
+    )
+})
+
+test_that("if the two rectangle is the same, the overlap area is the area of one of the rectangles", {
+    expect_equal(
+        areaOfOverlap(
+            list("bl" = c(x = 1, y = 1), "tr" = c(x = 4, y = 3)),
+            list("bl" = c(x = 1, y = 1), "tr" = c(x = 4, y = 3))
+        ),
+        6
+    )
+})
+
+test_that("overlap is calculated id x side has overlap, y side is the same", {
+    expect_equal(
+        areaOfOverlap(
+            list("bl" = c(x = 0, y = 0), "tr" = c(x = 3, y = 1)),
+            list("bl" = c(x = 2, y = 0), "tr" = c(x = 4, y = 1))
+        ),
+        1
+    )
+})
+
+test_that("overlap is calculated id x side has overlap, y side is the same and rectangles are in reverse order", {
+    expect_equal(
+        areaOfOverlap(
+            list("bl" = c(x = 2, y = 0), "tr" = c(x = 4, y = 1)),
+            list("bl" = c(x = 0, y = 0), "tr" = c(x = 3, y = 1))
+        ),
+        1
+    )
 })
