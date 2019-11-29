c74c8d51ef5f3b76c4ed66b9ab88f584d8a34598
Ildiko Czeller <czeildi@gmail.com>
2019-11-29 10:36:40 +0100

    solution

--- R/dojo.R
+++ R/dojo.R
@@ -1,1 +1,36 @@
 library("stringr")
+library("magrittr")
+parseMolecule <- function(molecule){
+    if (nchar(molecule) == 0) {
+        return(integer(0))
+    }
+    
+    molecule %>% 
+        splitMoleculeString() %>% 
+        lapply(function(atom_group) {
+            atom_name <- gsub("[0-9]+", "", atom_group)
+            createKeyValuePair(atom_name, getNumAtoms(atom_group))
+        }) %>% 
+        unlist()
+}
+
+splitMoleculeString <- function(molecule){
+    molecule %>%
+        gsub("([A-Z])","#\\1", .) %>%
+        strsplit(., "#") %>%
+        .[[1]] %>% 
+        .[2:length(.)]
+}
+
+getNumAtoms <- function(atomgroup) {
+    atomgroup %>% 
+        str_extract("[0-9]+") %>% 
+        as.numeric() %>% 
+        ifelse(is.na(.), 1, .)
+}
+
+createKeyValuePair <- function(key, value) {
+    result <- c(value)
+    names(result) <- key
+    result
+}
--- tests/testthat/test-dojo.R
+++ tests/testthat/test-dojo.R
@@ -1,5 +1,36 @@
 context("dojo")
 
-test_that("dummy", {
-    expect_true(TRUE)
+test_that("empty molecule has no atoms", {
+    expect_equal(
+        parseMolecule(""),
+        integer(0)
+    )
+})
+
+test_that("can parse one atom", {
+    expect_equal(
+        parseMolecule("H"),
+        c(H = 1)
+    )
+})
+
+test_that("can parse two single letter atoms", {
+    expect_equal(
+        parseMolecule("CO"),
+        c(C = 1, O = 1)
+    )  
+})
+
+test_that("can parse 2-letter single atom", {
+    expect_equal(
+        parseMolecule("HCl"),
+        c(H = 1, Cl = 1)
+    )
+})
+
+test_that("can parse atoms with numbers", {
+    expect_equal(
+        parseMolecule("H2"),
+        c(H = 2)
+    )
 })
