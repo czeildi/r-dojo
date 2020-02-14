49b902773c99fd1a9efe4dceb8db868e39acc39f
Ildiko Czeller <czeildi@gmail.com>
2020-02-14 15:41:10 +0100

    add last digit solution

--- R/dojo.R
+++ R/dojo.R
@@ -1,1 +1,45 @@
 library("stringr")
+
+lastDigit <- function(input) {
+    if (length(input) == 1) {
+        input %% 10
+    } else {
+        reduced_input <- reduceByLastExponentLevel(input)
+        lastDigit(reduced_input)
+    }
+}
+
+reduceByLastExponentLevel <- function(input) {
+    n <- length(input)
+    base <- input[n - 1] %% 10
+    exponent <- input[n]
+    
+    simplified_exponent <- simplifyExponent(base, exponent)
+    last_exponent <- base ^ simplified_exponent
+    
+    c(
+        input[seq_len(n - 2)],
+        last_exponent
+    )
+}
+
+simplifyExponent <- function(base_digit, exponent) {
+    cycle <- getCycleOfExponents(base_digit)
+    # remainder, but divisor instead of 0
+    (exponent %% length(cycle)) + length(cycle)
+}
+
+getCycleOfExponents <- function(digit) {
+    list(
+        c(0),
+        c(1),
+        c(2, 4, 8, 6),
+        c(3, 9, 7, 1),
+        c(4, 6),
+        c(5),
+        c(6),
+        c(7, 9, 3, 1),
+        c(8, 4, 2, 6),
+        c(9, 1)
+    )[[digit + 1]]
+}
--- tests/testthat/test-dojo.R
+++ tests/testthat/test-dojo.R
@@ -1,5 +1,43 @@
 context("dojo")
 
-test_that("dummy", {
-    expect_true(TRUE)
+test_that("returns number if input is single digit number", {
+    expect_equal(
+        lastDigit(6),
+        6
+    )
+})
+
+test_that("returns last digit of number if input is multiple digit number", {
+    expect_equal(
+        lastDigit(32),
+        2
+    )
+})
+
+test_that("returns last digit of power if input is 2 storey power", {
+    expect_equal(
+        lastDigit(c(9, 2)),
+        1
+    )
+})
+
+test_that("returns last digit of power if input is 3 storey power", {
+    expect_equal(
+        lastDigit(c(2, 2, 2)),
+        6
+    )
+})
+
+test_that("returns last digit of power if input is 2 storey power with large number in base", {
+    expect_equal(
+        lastDigit(c(1123456789, 2)),
+        1
+    )
+})
+
+test_that("returns last digit when 6 is raised to the power of a huge number", {
+    expect_equal(
+        lastDigit(c(6, 123456789)),
+        6
+    )
 })
